dfa6d815cc7a7f02f3a83940d06e935281d88b2f
Virkkunen <vrkknn@vrkknn.net>
2025-09-15 12:07:37 +0200

    restart

---
+++ app/src/main/java/net/vrkknn/andromuks/MainActivity.kt
@@ -0,0 +1,39 @@
+package net.vrkknn.andromuks
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import net.vrkknn.andromuks.ui.theme.AndromuksTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndromuksTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    AppNavigation(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppNavigation(modifier: Modifier) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "login", modifier = modifier) {
+        composable("login") { LoginScreen(navController = navController, modifier = modifier) }
+    }
+}
