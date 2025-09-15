29365207b7d7652e5e7982faf0c5aa667abb7bc3
Virkkunen <vrkknn@vrkknn.net>
2025-09-15 19:20:18 +0200

    changes

---
+++ app/src/main/java/net/vrkknn/andromuks/RoomItem.kt
@@ -0,0 +1,23 @@
+package net.vrkknn.andromuks
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Immutable
+import java.lang.reflect.Modifier
+
+// roomId: String
+// name: String
+// lastMessagePreview: String?
+// unreadCount: Int
+// avatarUrl: String?
+// isInvite: Boolean
+
+
+@Immutable
+data class RoomItem(
+    val id: String,
+    val name: String,
+    val messagePreview: String?,
+    val unreadCount: Int?,
+    val avatarUrl: String?,
+//    val isInvite: Boolean
+)
--- app/src/main/java/net/vrkknn/andromuks/RoomListScreen.kt
+++ app/src/main/java/net/vrkknn/andromuks/RoomListScreen.kt
@@ -1,14 +1,36 @@
 package net.vrkknn.andromuks
 
+import android.util.Log
 import android.view.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import net.vrkknn.andromuks.ui.theme.AndromuksTheme
 
+val mockRoomList = listOf(
+    RoomItem(id = "1", name = "There is a chat that never goes out", messagePreview = "last message", unreadCount = 1, avatarUrl = null),
+    RoomItem(id = "2", name = "Chatting up the hill", messagePreview = "last message", unreadCount = 0, avatarUrl = null),
+    RoomItem(id = "3", name = "Chat suey", messagePreview = "last message", unreadCount = 0, avatarUrl = null),
+    RoomItem(id = "4", name = "Chatter's delight", messagePreview = "last message", unreadCount = 12, avatarUrl = null),
+    RoomItem(id = "5", name = "Chat of glass", messagePreview = "last message", unreadCount = 0, avatarUrl = null)
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RoomListScreen(
     navController: NavController,
@@ -17,7 +39,35 @@ fun RoomListScreen(
 ) {
     AndromuksTheme {
         Surface {
-            Text("lul")
+            TopAppBar(
+                title = { Text("Chats") }
+            )
+            LazyColumn(
+                modifier.fillMaxSize().padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+            }
         }
+    }
+}
+@Composable
+fun RoomListItem(
+    room: RoomItem,
+    onRoomClick: (RoomItem) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onRoomClick(room) }
+            .padding(vertical = 12.dp, horizontal = 16.dp)
+    ) {
+        Text(
+            text = room.name,
+            style = MaterialTheme.typography.titleMedium
+        )
+        // rest of room details here
     }
 }
