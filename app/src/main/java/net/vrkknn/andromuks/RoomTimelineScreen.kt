a5060c5f6c37a2433d78418aa75d3ad2076597ce
ricardo-duarte-av <ricardo.duarte@aguiarvieira.pt>
2025-10-01 00:03:01 +0100

    Adjust bubble shape styling in TimelineEventItem for user and non-user messages. Updated corner radii to enhance visual consistency and improve layout aesthetics.

--- app/src/main/java/net/vrkknn/andromuks/RoomTimelineScreen.kt
+++ app/src/main/java/net/vrkknn/andromuks/RoomTimelineScreen.kt
@@ -674,13 +674,13 @@ fun TimelineEventItem(
                     val bubbleShape = if (isMine) {
                         RoundedCornerShape(
                             topStart = 16.dp,
-                            topEnd = 16.dp,
+                            topEnd = 2.dp,
                             bottomEnd = 8.dp,
                             bottomStart = 16.dp
                         )
                     } else {
                         RoundedCornerShape(
-                            topStart = 16.dp,
+                            topStart = 2.dp,
                             topEnd = 16.dp,
                             bottomEnd = 16.dp,
                             bottomStart = 8.dp
