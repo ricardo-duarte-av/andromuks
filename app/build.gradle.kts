0736a5dc3787090172f9235b2b4f14ad2218caea
Ricardo Duarte <ricardo.dra.vieira@gmail.com>
2026-02-20 21:46:52 +0000

    chore: update version to 1.0.29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
 
         // Update versionName for each release (e.g., 1.0, 1.1, 1.2, 2.0)
-        versionName = "1.0.28"
+        versionName = "1.0.29"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
