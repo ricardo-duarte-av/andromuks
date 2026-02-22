420a2ccebf2820c2a458bfd8458ba580ab29506c
Ricardo Duarte <ricardo.dra.vieira@gmail.com>
2026-02-22 18:29:44 +0000

    Delete and fix some files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.gms.google-services")
-    id("androidx.baselineprofile")
     alias(libs.plugins.baselineprofile)  // use alias
 }
 
--- baselineprofile/build/outputs/androidTest-results/connected/nonMinifiedRelease/GM1901 - 16/device-info.pb
+++
@@ -1,2 +0,0 @@
-
-a4a4334636ÀçÈÄ*	arm64-v8a*armeabi-v7a*armeabi2OnePlus:a4a43346RGM1901
