570b7492387747d2ae55d1cfe58c33d44759d4b8
Ricardo Duarte <ricardo.dra.vieira@gmail.com>
2026-02-28 19:48:16 +0000

    Update versionName to 1.0.41 for the next release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
 
         // Update versionName for each release (e.g., 1.0, 1.1, 1.2, 2.0)
-        versionName = "1.0.40"
+        versionName = "1.0.41"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
