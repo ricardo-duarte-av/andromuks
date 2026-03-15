725a759405413b93206b1d5342b1246642c4bec8
Ricardo Duarte <ricardo.dra.vieira@gmail.com>
2026-03-15 21:37:25 +0000

    chore: bump application version to 1.0.59

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
 
         // Update versionName for each release (e.g., 1.0, 1.1, 1.2, 2.0)
-        versionName = "1.0.58"
+        versionName = "1.0.59"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
