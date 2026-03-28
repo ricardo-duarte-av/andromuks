3b3b57f559e281f3fcb7e83d18ac933399e10b9f
ricardo-duarte-av <ricardo.dra.vieira@gmail.com>
2026-03-28 02:36:10 +0000

    chore: bump app version to 1.0.75

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
 
         // Update versionName for each release (e.g., 1.0, 1.1, 1.2, 2.0)
-        versionName = "1.0.74"
+        versionName = "1.0.75"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
