49759ed57d49019171e32a78df22596358d470a4
ricardo-duarte-av <ricardo.dra.vieira@gmail.com>
2026-04-07 19:53:36 +0100

    chore: bump version name to 1.0.81

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
 
         // Update versionName for each release (e.g., 1.0, 1.1, 1.2, 2.0)
-        versionName = "1.0.80"
+        versionName = "1.0.81"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
