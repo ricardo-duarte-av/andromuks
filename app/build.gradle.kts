19090057ab84724ba29522af16eff5cf927e521b
ricardo-duarte-av <ricardo.duarte@aguiarvieira.pt>
2026-04-16 21:39:55 +0100

    chore: increment app version name to 1.0.86

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
 
         // Update versionName for each release (e.g., 1.0, 1.1, 1.2, 2.0)
-        versionName = "1.0.85"
+        versionName = "1.0.86"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
