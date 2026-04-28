18b9cca3e186d9337fb6523ae46541df0ee0784b
ricardo-duarte-av <ricardo.duarte@aguiarvieira.pt>
2026-04-28 22:50:28 +0100

    chore: bump version name to 1.0.91

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
 
         // Update versionName for each release (e.g., 1.0, 1.1, 1.2, 2.0)
-        versionName = "1.0.90"
+        versionName = "1.0.91"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
