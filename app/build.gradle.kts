92bf95aca18294da89b22eeff9ea87a497184d85
Ricardo Duarte <ricardo.dra.vieira@gmail.com>
2026-08-11 16:15:42 +0000

    chore(release): 1.1.28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 
 
         // Update versionName for each release (e.g., 1.0, 1.1, 1.2, 2.0)
-        versionName = "1.1.27"
+        versionName = "1.1.28"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
