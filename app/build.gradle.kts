51fe31df32c835244b3bfdd9f013cdb4672af680
Ricardo Duarte <ricardo.dra.vieira@gmail.com>
2026-09-07 09:21:14 +0000

    chore(release): 1.1.46

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 
 
         // Update versionName for each release (e.g., 1.0, 1.1, 1.2, 2.0)
-        versionName = "1.1.45"
+        versionName = "1.1.46"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
